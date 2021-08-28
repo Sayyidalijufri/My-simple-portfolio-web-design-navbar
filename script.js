7b9aa369e39a155ebfc31518941748ac981629b4
Sayyid ali aljufri <88267951+Sayyidalijufri@users.noreply.github.com>
2021-08-28 08:05:08 +0000

    maked a scroll-lock class

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const menuToggle = document.querySelector(`#menu-toggle input`)
 const nav = document.querySelector('nav ul');
-
+const body = document.querySelector('body');
 
 
 menuToggle.addEventListener('click', function() {
-    nav.classList.toggle('active')
+    nav.classList.toggle('slide')
+    body.classList.toggle('scroll-lock')
 });
