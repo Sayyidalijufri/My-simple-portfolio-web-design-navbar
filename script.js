c9593b679569b8ab3a86216a1bbd6b294457099b
Sayyid ali aljufri <88267951+Sayyidalijufri@users.noreply.github.com>
2021-08-28 06:58:16 +0000

    i had maked tog

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector(`#menu-toggle input`)
+const nav = document.querySelector('nav ul');
+
+
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide')
+});
