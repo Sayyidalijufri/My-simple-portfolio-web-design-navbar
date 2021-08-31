555c3435050fee3794a40ba7919f3bb9dbf7c875
Sayyid ali aljufri <88267951+Sayyidalijufri@users.noreply.github.com>
2021-08-31 04:07:19 +0000

    make the animation on hover in navbar

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const menuToggle = document.querySelector(`#menu-toggle input`)
 const nav = document.querySelector('nav ul');
 const body = document.querySelector('body');
+const menuspan = document.querySelectorAll('#menu-toggle span')
+const menulink = document.querySelectorAll('nav ul li a')
 
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('active');
     body.classList.toggle('scroll-lock');
 });
+
+menulink.forEach((link) => link.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  document.querySelector('#nav #menu-toggle input').checked = false;
+}));
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    body.classList.remove('scroll-lock');
+  }
+  else if (nav.classList.contains('active')){
+    body.classList.add('scroll-lock');
+  }
+}
+
+var x = window.matchMedia("(min-width: 467px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
